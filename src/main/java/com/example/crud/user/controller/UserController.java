72db15bdcaa2e66f515b1bab5bca830554c51bdc
LakshanAmal <lakshanamal100@gmail.com>
2021-10-11 09:18:47 +0530

    add logger msg and bug fixed

--- src/main/java/com/example/crud/user/controller/UserController.java
+++ src/main/java/com/example/crud/user/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.example.crud.user.controller;
 
-import com.example.crud.user.dto.UserDTO;
 import com.example.crud.user.model.User;
 import com.example.crud.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 @RequestMapping("api/v1")
 public class UserController {
     private final UserService userService;
+    Logger log= LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService){
         this.userService=userService;
@@ -20,26 +22,31 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
+        log.info("Get request received for get all users");
         return ResponseEntity.ok(userService.getUsers());
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUser(@PathVariable String id){
+        log.info("Get request received for get user, user_id="+id);
         return ResponseEntity.ok(userService.getUser(id));
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Map<String,String>> saveUser(@RequestBody User User){
-        return ResponseEntity.ok(userService.saveUser(User));
+    public ResponseEntity<Map<String,String>> saveUser(@RequestBody User user){
+        log.info("Post request received , user_id="+user.getId());
+        return ResponseEntity.ok(userService.saveUser(user));
     }
 
     @PutMapping("/users/{id}")
     public ResponseEntity<Map<String,String>> updateUserPut(@RequestBody User user, @PathVariable String id){
+        log.info("Put request received , user_id="+user.getId());
         return ResponseEntity.ok(userService.updateUser(user,id));
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<Map<String,String>> deleteUser(@PathVariable String id){
+        log.info("Delete request received , user_id="+id);
         return ResponseEntity.ok(userService.deleteUser(id));
     }
 }
--- src/main/java/com/example/crud/user/service/UserService.java
+++ src/main/java/com/example/crud/user/service/UserService.java
@@ -4,6 +4,8 @@ import com.example.crud.user.exception.UserAlreadyInException;
 import com.example.crud.user.exception.UserNotFoundException;
 import com.example.crud.user.model.User;
 import com.example.crud.user.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,50 +14,61 @@ import java.util.*;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    Logger log= LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository=userRepository;
     }
 
-    public List<User> getUsers() throws RuntimeException {
+    public List<User> getUsers(){
+        log.info("Get request completed");
         return userRepository.findAll();
     }
 
     public User getUser(String id) {
         if(userRepository.findById(id).isEmpty()){
+            log.error("User not found, user_id="+id);
             throw new UserNotFoundException("User not found, user_id="+id);
         }
+        log.info("Get request completed");
             return userRepository.findById(id).get();
     }
 
     public Map<String,String> saveUser(User user) {
        if(userRepository.findById(user.getId()).isPresent()){
+           log.error("User already in system, user_id="+user.getId());
            throw new UserAlreadyInException("User already in system, user_id="+user.getId());
        }
         userRepository.save(user);
         Map<String,String> map = new HashMap<String,String>();
         map.put("msg","New user added, user_id="+user.getId());
+        log.info("Post request completed");
         return  map;
     }
 
     public Map<String,String> updateUser(User user,String id) {
         if(userRepository.findById(id).isEmpty()){
-            throw new UserNotFoundException("User not exits in the database");
+            log.error("User not exits in the database");
+            throw new UserNotFoundException("User not exists in the database");
         }
+
         userRepository.save(user);
         Map<String,String> map = new HashMap<String,String>();
         map.put("msg","Update user successful, user_id="+id);
+        log.info("Put request completed");
         return map;
     }
 
     public Map<String, String>  deleteUser(String id) {
         if(userRepository.findById(id).isEmpty()){
-            throw new UserNotFoundException("User not exits in the database");
+            log.error("User not exits in the database");
+            throw new UserNotFoundException("User not exists in the database");
         }
         userRepository.deleteById(id);
         Map<String,String> map = new HashMap<String,String>();
         map.put("msg","User delete successful, user_id="+id);
+        log.info("Delete request completed");
         return map;
     }
 
