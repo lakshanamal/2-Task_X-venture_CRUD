102eb6cf33b58f20f12699c4aba3208caa9dd5bf
LakshanAmal <lakshanamal100@gmail.com>
2021-10-14 09:33:24 +0530

    improve code adding operation

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,11 +1,13 @@
 package com.example.crud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
+@OpenAPIDefinition
 public class CrudApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/crud/user/controller/UserController.java
+++ src/main/java/com/example/crud/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.crud.user.controller;
 
 import com.example.crud.user.model.User;
 import com.example.crud.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin
 public class UserController {
     private final UserService userService;
     Logger log= LoggerFactory.getLogger(UserController.class);
@@ -20,30 +22,34 @@ public class UserController {
         this.userService=userService;
     }
 
+    @Operation(summary="Get all user data")
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
         log.info("Get request received for get all users");
         return ResponseEntity.ok(userService.getUsers());
     }
-
+    @Operation(summary="Get user data by id")
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUser(@PathVariable String id){
         log.info("Get request received for get user, user_id="+id);
         return ResponseEntity.ok(userService.getUser(id));
     }
 
+    @Operation(summary="Save userdata to database")
     @PostMapping("/users")
     public ResponseEntity<Map<String,String>> saveUser(@RequestBody User user){
         log.info("Post request received , user_id="+user.getId());
         return ResponseEntity.ok(userService.saveUser(user));
     }
 
+    @Operation(summary="Update user by id")
     @PutMapping("/users/{id}")
     public ResponseEntity<Map<String,String>> updateUserPut(@RequestBody User user, @PathVariable String id){
         log.info("Put request received , user_id="+user.getId());
         return ResponseEntity.ok(userService.updateUser(user,id));
     }
 
+    @Operation(summary="Delete user by id")
     @DeleteMapping("/users/{id}")
     public ResponseEntity<Map<String,String>> deleteUser(@PathVariable String id){
         log.info("Delete request received , user_id="+id);
